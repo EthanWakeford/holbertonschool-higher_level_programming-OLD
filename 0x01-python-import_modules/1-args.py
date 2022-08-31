18f84e73e4af861c94af4495d4c7259825ab106d
EthanWakeford <ethanwakeford@gmail.com>
2022-08-31 12:04:21 -0500

    updated 1 to not run when importing

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 from sys import argv
-if len(argv) == 1:
-    print("0 arguments")
-    exit()
-count = 1
-print("{} argument".format(len(argv)-1), "s"if len(argv) > 1 else None, sep="")
-for i in argv[1:]:
-    print("{}: {}".format(count, argv[count]))
-    count += 1
+
+
+def main():
+    if len(argv) == 1:
+        print("0 arguments")
+        exit()
+    count = 1
+    print("{} argument".format(len(argv)-1),  end="")
+    print("s" if len(argv) > 2 else None)
+    for i in argv[1:]:
+        print("{}: {}".format(count, argv[count]))
+        count += 1
+if __name__ == "__main__":
+    main()
