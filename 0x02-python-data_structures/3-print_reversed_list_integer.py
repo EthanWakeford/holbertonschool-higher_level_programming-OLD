c4cec8661322ed552dba6fe7336bb40cf85251a5
EthanWakeford <ethanwakeford@gmail.com>
2022-09-01 11:11:19 -0500

    typecast to integer in print idk

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     list = []
     for i in range(len(my_list) - 1, -1, -1):
         list.append(my_list[i])
-    print("{}".format(list))
+    print("{:d}".format(list))
