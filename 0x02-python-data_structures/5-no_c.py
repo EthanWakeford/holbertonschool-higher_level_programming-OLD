42ff53d46bfaba2e22623a1a95b1a47a97e3d8d7
EthanWakeford <ethanwakeford@gmail.com>
2022-09-01 11:46:10 -0500

    added 5

---
+++ 0x02-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    list = []
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            list.append(i)
+    str = ''.join(list)
+    return (str)
