bb24c0c0b596ffae70c101cefbaadc8ecf978adb
EthanWakeford <ethanwakeford@gmail.com>
2022-09-06 14:32:08 -0500

    added 12

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) == str or not roman_string:
+    if type(roman_string) != str or not roman_string:
         return (0)
     sum = 0
     values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
