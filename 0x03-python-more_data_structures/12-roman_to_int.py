7be9fbd3dd2950ca1715524834cc24f5005576d7
EthanWakeford <ethanwakeford@gmail.com>
2022-09-06 14:26:41 -0500

    added 12

---
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sum = 0
+    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+    'D': 500, 'M': 1000}
+    last = 0
+    for i in range(len(roman_string)):
+        if last != 0 and last < values[roman_string[i]]:
+            sum -= last
+        else:
+            sum += last
+        last = values[roman_string[i]]
+    sum += last
+    return (sum)
