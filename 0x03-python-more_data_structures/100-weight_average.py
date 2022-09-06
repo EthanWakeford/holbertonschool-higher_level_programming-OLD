e441c2d8c4453bfd4a1fad3688cbb1813c2b4c52
EthanWakeford <ethanwakeford@gmail.com>
2022-09-06 14:56:35 -0500

    now returns 0 if list is empty

--- 0x03-python-more_data_structures/100-weight_average.py
+++ 0x03-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return (0)
     products = [(x * y) for x, y in my_list]
     divides = [(y) for x, y in my_list]
     return (sum(products) / sum(divides))
