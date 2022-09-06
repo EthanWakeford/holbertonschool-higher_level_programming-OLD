9cb1cd299aa52677f7ff149db25e3f278a4a1582
EthanWakeford <ethanwakeford@gmail.com>
2022-09-06 13:07:06 -0500

    added 4

---
+++ 0x03-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    diff_1 = list(filter(lambda x: x not in set_2, set_1))
+    diff_2 = list(filter(lambda x: x not in set_1, set_2))
+    diff = diff_1 + diff_2
+    return (diff)
