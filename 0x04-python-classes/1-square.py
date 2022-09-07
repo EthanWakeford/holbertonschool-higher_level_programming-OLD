e66a6a5b3171eb929e507d4385a5360edbce3e9a
EthanWakeford <ethanwakeford@gmail.com>
2022-09-07 10:04:39 -0500

    added quotes

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -13,6 +13,6 @@ class Square:
     @size.setter
     def size(self, size):
         if not isinstance(size, int):
-            raise TypeError(size must be an integer)
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError(size must be >= 0)
+            raise ValueError("size must be >= 0")
