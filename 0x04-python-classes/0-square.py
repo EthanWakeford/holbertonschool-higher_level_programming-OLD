d8e5d80c14a4baaf5c3844dfc0f01097d7346077
EthanWakeford <ethanwakeford@gmail.com>
2022-09-07 09:43:58 -0500

    added documentation

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" This module is used to create a class Square"""
 class Square:
+    """square class definition"""
     def __init__(self, size):
+        """init method for self.size attribute
+        args:
+            size (int): size of square
+        """
         self.__size = size
