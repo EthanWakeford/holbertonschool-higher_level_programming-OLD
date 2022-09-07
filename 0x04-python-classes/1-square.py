22a092cdf933a23cd1fb778e5640505d82036302
EthanWakeford <ethanwakeford@gmail.com>
2022-09-07 10:21:09 -0500

    updated 1 for pep8

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -3,16 +3,20 @@
 
 
 class Square:
-    """this class is used to create a square with a size attribute"""   
+    """this class is used to create a square with a size attribute"""
     def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         """creates a size attribute >= 0"""
         return self.__size
+
     @size.setter
     def size(self, size):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
