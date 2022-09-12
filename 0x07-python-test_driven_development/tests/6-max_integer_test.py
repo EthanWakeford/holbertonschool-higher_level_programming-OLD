84e959aeab1c2063b4e9fdff5c455d24cdc7e95c
EthanWakeford <ethanwakeford@gmail.com>
2022-09-12 17:38:46 -0500

    updated for linter

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,16 +4,15 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     '''class to test max_integer function'''
-
 
     def test_valid(self):
         '''tests valid inputs'''
         self.assertEqual(max_integer([1, 5, 3]), 5)
         self.assertEqual(max_integer([4]), 4)
         self.assertEqual(max_integer(), None)
-
 
     def test_invalid(self):
         '''tests invalid inputs'''
