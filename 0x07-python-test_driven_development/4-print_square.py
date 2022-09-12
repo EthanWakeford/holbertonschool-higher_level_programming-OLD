9d3fde825fc89a38b230c7cb03173f68fa90d9c0
EthanWakeford <ethanwakeford@gmail.com>
2022-09-12 11:01:50 -0500

    added 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+'''this module is used to practice doctests in python'''
+
+
+def print_square(size):
+    '''function prints a square with sides of size ~size~'''
+    if type(size) == float:
+        size = int(size)
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    
+    for i in range(size):
+        print("#" * size)
