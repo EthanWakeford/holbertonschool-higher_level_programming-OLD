bc99b5bc44159a297774c2646eaf624b5bd46e1a
EthanWakeford <ethanwakeford@gmail.com>
2022-09-13 12:04:18 -0500

    changed for pep8

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle():
 
     @width.setter
     def width(self, width):
-        if type(value) != int:
+        if type(width) != int:
             raise TypeError('width must be an integer')
         elif width < 0:
             raise ValueError('width must be >= 0')
@@ -32,7 +32,7 @@ class Rectangle():
     def height(self, height):
         if type(height) != int:
             raise TypeError('height must be an integer')
-        elif height <0 :
+        elif height < 0:
             raise ValueError('height must be >= 0')
         else:
             self.__height = height
