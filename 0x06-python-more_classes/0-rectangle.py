6a96702e24feb6a9909b4315c26c85bf41920b05
EthanWakeford <ethanwakeford@gmail.com>
2022-09-13 11:58:05 -0500

    added 0

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""this module has a class Rectangle"""
+
+
+class Rectangle():
+    """class rectangle with height and width properties"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """gets the width of the rectangle"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = width
+
+    @property
+    def height(self):
+        """height of the rectangle"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(height) != int:
+            raise TypeError('height must be an integer')
+        elif height <0 :
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = height
