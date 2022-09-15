1751f28e286605e1b0eba0986fb4260fe2fa1910
EthanWakeford <ethanwakeford@gmail.com>
2022-09-15 11:08:56 -0500

    added 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module to return all methods and attriutes of an object"""
+
+
+def lookup(obj):
+    """returns all methods and attributes of an object"""
+    return (dir(object))
