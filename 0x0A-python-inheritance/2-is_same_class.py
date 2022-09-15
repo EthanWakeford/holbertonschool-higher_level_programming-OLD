5e877506438acc01df5e9a80c19c8e7b730229e5
EthanWakeford <ethanwakeford@gmail.com>
2022-09-15 13:03:25 -0500

    added 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""has a function that checks type of object"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true if object is of specified class"""
+    return (isinstance(obj, a_class))
