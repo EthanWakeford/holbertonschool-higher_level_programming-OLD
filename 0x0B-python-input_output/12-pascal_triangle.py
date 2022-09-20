84889ecb0ece56545381481ce4bf428dc182a8a5
EthanWakeford <ethanwakeford@gmail.com>
2022-09-19 23:36:34 -0500

    added 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""pascals triangle"""
+
+
+def pascal_triangle(n): 
+    """creates a pascals triangle"""
+
+    if n <= 0:
+        return ([])
+    big_list = [[1]]
+    l1 = [1]
+    for i in range(1, n):
+        l2 = [1]
+        for j in range(len(l1)):
+            if j < len(l1) - 1:
+                l2.append(l1[j] + l1[j + 1])
+        l2.append(1)
+        print(l2)
+        big_list.append(l2)
+        l1 = l2
+    print("_" * 10)
+    return (big_list)
