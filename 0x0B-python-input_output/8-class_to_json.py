776425efc6b9a3842fe7a4a60e7dbd70deb37a1d
EthanWakeford <ethanwakeford@gmail.com>
2022-09-19 22:25:19 -0500

    added 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,11 +5,4 @@
 def class_to_json(obj):
     """Puts all attributes of an object into a dictionary"""
 
-    att_dict = {}
-    valid_types = ['list', 'dict', 'str', 'int', 'bool']
-    obj_attributes = dir(obj)
-    for i in obj_attributes:
-        if type(i) in valid_types:
-            att_dict[type[i]] = i
-
-    return (att_dict)
+    return (vars(obj))
