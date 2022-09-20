68bf59303a47bed31ed55646c2169d2e0d78b09b
EthanWakeford <ethanwakeford@gmail.com>
2022-09-19 21:54:57 -0500

    added 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that adds all arguments to a python list then
+saves to a json file"""
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def main():
+    """script stuff"""
+
+    from sys import argv
+    try:
+        lst = (load_from_json_file('add_item.json'))
+    except FileNotFoundError:
+        lst = []
+    save_to_json_file(lst + argv[1:], 'add_item.json')
+
+if __name__ == "__main__":
+    main()
