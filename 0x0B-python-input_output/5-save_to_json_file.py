5140336d3db7c116ec1c20056d80f6e0ce756fca
EthanWakeford <ethanwakeford@gmail.com>
2022-09-19 21:20:21 -0500

    added 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""writes a python object to a json file"""
+
+
+def save_to_json_file(my_obj, filename):
+    """writes a python object to a json file"""
+
+    import json
+    with open(filename, 'w', encoding='utf-8'):
+        f.write(json.dumps(my_obj))
