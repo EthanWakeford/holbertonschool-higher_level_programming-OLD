2395db701d0d58aba28dba7a0b69fec73a0e60a2
EthanWakeford <ethanwakeford@gmail.com>
2022-09-19 21:12:05 -0500

    added 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""returns a python object from json string"""
+
+
+def from_json_string(my_str):
+    """returns a python object from json string"""
+
+    return (json.loads(my_str))
