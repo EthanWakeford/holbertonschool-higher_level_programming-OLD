044effd080eeed3bbbf7f083be96097faa0fc221
EthanWakeford <ethanwakeford@gmail.com>
2022-09-19 21:00:54 -0500

    copy pasting code that does something else can be bad

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """contains a function to append a string to a text file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """appends a string to a text file"""
 
     with open(filename, 'a', encoding='utf-8') as f:
