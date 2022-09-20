42e92fd32cca8cc35be7ab0b0eb73d182e1f2d62
EthanWakeford <ethanwakeford@gmail.com>
2022-09-19 20:54:09 -0500

    added 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""contains a function to write a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file"""
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
+
+    return (len(text))
