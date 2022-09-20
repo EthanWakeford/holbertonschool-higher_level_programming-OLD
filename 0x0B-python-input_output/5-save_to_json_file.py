5a1652e9c98ebf3fd2a28181f44cbaacfd21c26c
EthanWakeford <ethanwakeford@gmail.com>
2022-09-19 21:21:50 -0500

    declared f variable

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,5 +6,5 @@ def save_to_json_file(my_obj, filename):
     """writes a python object to a json file"""
 
     import json
-    with open(filename, 'w', encoding='utf-8'):
+    with open(filename, 'w', encoding='utf-8') as f:
         f.write(json.dumps(my_obj))
