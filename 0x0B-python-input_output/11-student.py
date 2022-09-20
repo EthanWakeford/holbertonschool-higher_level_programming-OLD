a6284468cbe80c27c283288ee5c05b760a80ae40
EthanWakeford <ethanwakeford@gmail.com>
2022-09-19 23:13:29 -0500

    added 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Has a class student"""
+
+
+class Student:
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictionary representation of class"""
+
+        if attrs is None:
+            return (vars(self))
+        attributes = {}
+        for key, value in vars(self).items():
+            if key in attrs:
+                attributes[key] = value
+        return (attributes)
+
+    def reload_from_json(self, json):
+        """replaces all attributes with data from json file"""
+
+        for key, value in json.items():
+            setattr(self, key, value)
