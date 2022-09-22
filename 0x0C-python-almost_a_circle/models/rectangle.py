7d5bfe74ec44b961ec371114859a7943a7f801db
EthanWakeford <ethanwakeford@gmail.com>
2022-09-21 22:36:27 -0500

    edited for pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,5 +96,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """returns all attributes in dictionary format"""
-        return ({'x': self.x, 'y': self.y, 'id': self.id, 'height': self.height,
-        'width': self.width})
+        return ({'x': self.x, 'y': self.y, 'id': self.id,
+                'height': self.height, 'width': self.width})
