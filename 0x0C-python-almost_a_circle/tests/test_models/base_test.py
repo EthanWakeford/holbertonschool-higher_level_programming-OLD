d3ead51b3a9fa91ab752ff06561f536285c3420f
EthanWakeford <ethanwakeford@gmail.com>
2022-09-23 15:44:00 -0500

    added test for documentation

--- 0x0C-python-almost_a_circle/tests/test_models/base_test.py
+++ 0x0C-python-almost_a_circle/tests/test_models/base_test.py
@@ -8,11 +8,27 @@ import unittest
 class TestBase(unittest.TestCase):
     """base class testing"""
 
+    def test_docs(self):
+        modDocstring = __import__('models').base.__doc__
+        self.assertIsNotNone(modDocstring)
+        self.assertIsNotNone(Base.__doc__)
+        self.assertIsNotNone(Base.to_json_string.__doc__)
+        self.assertIsNotNone(Base.save_to_file.__doc__)
+        self.assertIsNotNone(Base.from_json_string.__doc__)
+        self.assertIsNotNone(Base.create.__doc__)
+        self.assertIsNotNone(Base.load_from_file.__doc__)
+
+
     def test_init(self):
+        """tests init method"""
         b1 = Base()
         b2 = Base()
+        b3 = Base(12)
+        b4 = Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 12)
+        self.assertEqual(b4.id, 3)
 
 if __name__ == "__main__":
     unittest.main()
