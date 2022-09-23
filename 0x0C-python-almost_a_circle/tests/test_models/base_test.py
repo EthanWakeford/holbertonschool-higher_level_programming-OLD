cc99aa19f1692437e4877f2a16c9425669714352
EthanWakeford <ethanwakeford@gmail.com>
2022-09-23 16:16:04 -0500

    added test for to_json_string

--- 0x0C-python-almost_a_circle/tests/test_models/base_test.py
+++ 0x0C-python-almost_a_circle/tests/test_models/base_test.py
@@ -24,11 +24,18 @@ class TestBase(unittest.TestCase):
         b1 = Base()
         b2 = Base()
         b3 = Base(12)
-        b4 = Base()
+        b4 = Base(-2)
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
         self.assertEqual(b3.id, 12)
-        self.assertEqual(b4.id, 3)
+        self.assertEqual(b4.id, -2)
+
+    
+    def test_to_json_string(self):
+        self.assertEqual(Base.to_json_string(None), '[]')
+        self.assertEqual(Base.to_json_string([{'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}]), \
+            '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]')
+
 
 if __name__ == "__main__":
     unittest.main()
