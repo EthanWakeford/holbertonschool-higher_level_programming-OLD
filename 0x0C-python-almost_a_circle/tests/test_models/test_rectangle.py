f141f5d00d6f4ee9b407a524a658950473949be0
EthanWakeford <ethanwakeford@gmail.com>
2022-09-23 22:59:54 -0500

    testtst

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -81,3 +81,11 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1.__str__(), '[Rectangle] (89) 3/1 - 2/10')
         r1.update(x=1, height=2, y=3, width=4)
         self.assertEqual(r1.__str__(), '[Rectangle] (89) 1/3 - 4/2')
+
+    def test_to_dictionary(self):
+        r1 = Rectangle(10, 2, 1, 9)
+        self.assertEqual(r1.to_dictionary(), {'x': 1, 'y': 9, 'id': 13, 'height': 2, 'width': 10})
+        r2 = Rectangle(1, 1)
+        r2.update(**(r1.to_dictionary()))
+        self.assertEqual(r2.__str__(), '[Rectangle] (13) 1/9 - 10/2')
+        self.assertIsNot(r1, r2)
