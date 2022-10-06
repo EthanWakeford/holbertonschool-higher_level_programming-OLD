a375137328ffacf4eb0c004c4e175a6dbd361983
EthanWakeford <ethanwakeford@gmail.com>
2022-10-05 22:43:39 -0500

    added 15

--- 0x00-SQL_introduction/15-groups.sql
+++ 0x00-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of recors with same score
 SELECT SCORE, COUNT(score) AS number
-    FROM second_table 
-    GROUP BY score
-    ORDER BY COUNT(score) Desc;
+FROM second_table 
+GROUP BY score
+ORDER BY COUNT(score) Desc;
