fc91ceb9bbc5cbb91a0460c4f5ffc1a74fb675a0
EthanWakeford <ethanwakeford@gmail.com>
2022-10-06 11:02:25 -0500

    added comment

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
+-- creates a user with full privileges
 CREATE USER IF NOT EXISTS '1-create_user.sql'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO '1-create_user.sql'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
