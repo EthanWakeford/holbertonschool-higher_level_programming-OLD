1c0799e846c6bc65c662463efe63d88438d970b1
EthanWakeford <ethanwakeford@gmail.com>
2022-10-06 13:14:35 -0500

    why

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities in database
-SELECT cities.id, cities. name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states ON cities.state_id=states.id;
