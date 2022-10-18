9b01e9ae881c6bafcc4be10127a0c30804927e85
EthanWakeford <ethanwakeford@gmail.com>
2022-10-18 12:22:28 -0500

    added 1

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ def main(argv):
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states SORT BY id ASC")
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
         # check if first letter of name starts with 'N'
