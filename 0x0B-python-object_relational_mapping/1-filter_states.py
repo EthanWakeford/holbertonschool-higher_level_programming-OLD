d4a422792375d4231393cbdcb3e076dc8ee48ab6
EthanWakeford <ethanwakeford@gmail.com>
2022-10-18 12:10:45 -0500

    added 1

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""uses mysqldb to print all states from database where name starts with capital N"""
+import MySQLdb
+import sys
+
+
+def main(argv):
+    """main function to guard content"""
+    if len(argv) != 4:
+        return ()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][1] == 'N':
+            print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
