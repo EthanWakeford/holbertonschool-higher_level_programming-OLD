6844fe8c761fce08dfb7935f3a9bff00b00b2354
EthanWakeford <ethanwakeford@gmail.com>
2022-10-18 12:25:57 -0500

    added 2

---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""uses mysqldb to print all states from database where
+ name matches the argument"""
+import MySQLdb
+import sys
+
+
+def main(argv):
+    """main function to guard content"""
+    if len(argv) != 5:
+        return ()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name = {argv[4]}")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
