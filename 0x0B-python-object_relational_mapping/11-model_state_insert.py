80f5dd7207d3ac42815c0f26f9fa08dc91079c17
EthanWakeford <ethanwakeford@gmail.com>
2022-10-20 14:30:43 -0500

    added 11

---
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Start link class to table in database."""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        session.add(State(name='Louisiana'))
+        session.commit()
+        result = session.query(State.id).where(State.name == 'Louisiana').one()
+        print(result.id)
