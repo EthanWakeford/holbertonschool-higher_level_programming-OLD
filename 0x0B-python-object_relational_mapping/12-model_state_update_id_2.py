9bd547f4ae53da01d70926b5fbf1e367fba65b5c
EthanWakeford <ethanwakeford@gmail.com>
2022-10-20 16:07:33 -0500

    added 12

---
+++ 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Start link class to table in database."""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sqlalchemy import update
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        result = update(State).where(State.id == 2).values(name="New Mexico")
+        session.execute(result)
