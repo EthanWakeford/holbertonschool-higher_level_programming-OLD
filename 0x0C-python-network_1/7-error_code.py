d9b6d80d568888704a856ce017efde3bd0e2f7d2
EthanWakeford <ethanwakeford@gmail.com>
2022-11-01 16:13:37 -0500

    added 7

---
+++ 0x0C-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""guards for errors"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    r = requests.get(url)
+
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
