9db5772e00465d225406bf6ec306886347983cd1
EthanWakeford <ethanwakeford@gmail.com>
2022-11-01 12:21:32 -0500

    changed url

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -4,6 +4,6 @@
 
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('http://python.org/') as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print(html)
