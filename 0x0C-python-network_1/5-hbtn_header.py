7c8afa911cb66eea0ae9f4e882b4cf9dab5f31c0
EthanWakeford <ethanwakeford@gmail.com>
2022-11-01 15:10:07 -0500

    added 5

---
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""sends request and prints response"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    r = requests.get(url)
+    
+    print(r.headers.get('X-Request-Id'))
