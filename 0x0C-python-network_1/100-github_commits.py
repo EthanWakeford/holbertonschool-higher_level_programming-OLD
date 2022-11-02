669fb96eaf06982d5937c867fdc01190dc3c360b
EthanWakeford <ethanwakeford@gmail.com>
2022-11-01 21:02:37 -0500

    made it pretty

--- 0x0C-python-network_1/100-github_commits.py
+++ 0x0C-python-network_1/100-github_commits.py
@@ -8,9 +8,7 @@ if __name__ == "__main__":
     repo = argv[1]
     owner = argv[2]
 
-    payload = {}
-    payload['per_page'] = 10
-    payload['page'] = 1
+    payload = {'per_page': 10, 'page': 1}
 
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(owner, repo), params=payload)
@@ -19,8 +17,7 @@ if __name__ == "__main__":
 
     for commit in r.json():
         sha = commit.get("sha")
-        author = commit.get("commit", {}).get("author", {}).get("name")
+        author = commit.get("commit").get("author").get("name")
         commit_list.append("{}: {}".format(sha, author))
 
-    for i in commit_list:
-        print(i)
+    print(*commit_list, sep='\n')
