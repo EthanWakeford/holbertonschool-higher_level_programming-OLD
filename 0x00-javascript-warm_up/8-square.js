094d7d7fddd7b3a55fc8d26e0952c5a22d3c2fdb
EthanWakeford <ethanwakeford@gmail.com>
2022-12-01 14:36:54 -0600

    added 8

---
+++ 0x00-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (!parseInt(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let x = 0; x < parseInt(process.argv[2]); x++) {
+    console.log('x'.repeat(process.argv[2]));
+  }
+}
