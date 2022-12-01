8bf78e58b1af0f78085e173bd74af603f77e9f3a
EthanWakeford <ethanwakeford@gmail.com>
2022-12-01 15:27:21 -0600

    added 11

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (!parseInt(process.argv[3])) {
+if (!process.argv[3]) {
   console.log(0);
 } else {
-  console.log(process.argv.slice(2).sort()[1]);
+  console.log(process.argv.slice(2).sort().reverse()[1]);
 }
