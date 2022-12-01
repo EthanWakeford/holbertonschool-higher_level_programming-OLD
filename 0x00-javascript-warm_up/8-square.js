987f140a2414d7c404d5bf7ae466fcc2908ad777
EthanWakeford <ethanwakeford@gmail.com>
2022-12-01 14:38:19 -0600

    missing size

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (!parseInt(process.argv[2])) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let x = 0; x < parseInt(process.argv[2]); x++) {
     console.log('X'.repeat(process.argv[2]));
