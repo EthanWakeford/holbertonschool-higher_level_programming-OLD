0895c4ea5e32c726b367851b8cdd44f4e242a6dd
EthanWakeford <ethanwakeford@gmail.com>
2022-12-06 15:33:24 -0600

    added 4

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,15 @@
 const request = require('request');
 
 const url = process.argv[2];
-const wedgeId = 'https://swapi-api.hbtn.io/api/people/18/';
 
 request(url, (error, response, body) => {
   if (error) throw error;
   let antillesCount = 0;
-  for (const movie of ((JSON.parse(body)).results)) {
-    if (movie.characters.includes(wedgeId)) {
-      antillesCount++;
+  for (const movie of JSON.parse(body).results) {
+    for (const char of movie.characters) {
+      if (char.includes('/18/')) {
+        antillesCount++;
+      }
     }
   }
   console.log(antillesCount);
